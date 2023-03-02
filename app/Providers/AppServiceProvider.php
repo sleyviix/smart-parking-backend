571b23a7fed5dc8c9bd67a12b2b09f93227d35ab
sleyviix <190028486@aston.ac.uk>
2023-03-02 19:24:32 +0000

    -hosting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,17 +5,24 @@ namespace App\Providers;
 use App\Models\Reservation;
 use App\Observers\ReservationObserver;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
+
 {
     /**
      * Register any application services.
      *
      * @return void
      */
+//    public function register()
+//    {
+//        //
+//    }
+
     public function register()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**
