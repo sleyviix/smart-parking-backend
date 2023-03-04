1b164b135828f27dbcab42f988253cd63ecfee6b
sleyviix <190028486@aston.ac.uk>
2023-03-04 10:35:03 +0000

    -Hosting

--- database/migrations/2023_01_24_182943_rename_table.php
+++ database/migrations/2023_01_24_182943_rename_table.php
@@ -1,31 +1,31 @@
 <?php
-//
-//use Illuminate\Database\Migrations\Migration;
-//use Illuminate\Database\Schema\Blueprint;
-//use Illuminate\Support\Facades\Schema;
-//
-//return new class extends Migration
-//{
-//    /**
-//     * Run the migrations.
-//     *
-//     * @return void
-//     */
-//    public function up()
-//    {
-//        //
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
 //        Schema::table('reservations', function ($table) {
 //            $table->integer('paid_amount')->nullable()->unsigned()->comment('GBP');
 //        });
-//    }
-//
-//    /**
-//     * Reverse the migrations.
-//     *
-//     * @return void
-//     */
-//    public function down()
-//    {
-//        //
-//    }
-//};
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
